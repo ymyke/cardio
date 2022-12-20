b9db386523550892f4653795e5d78367ebc61d7e
ymyke <ymyke@users.noreply.github.com>
2022-12-20 19:26:45 +0100

    Finetune fights test framework, add one more test

--- tests/test_individual_fights.py
+++ tests/test_individual_fights.py
@@ -1,30 +1,34 @@
 from cardio import Card, Sigil, session, handlers
 
-# @pytest.mark.parametrize(
-#     "source, symbol",
-#     [
-#         ("coingecko", "ETH"),
-#     ],
-# )
-# @pytest.mark.net
-# def test_working_symbols_are_returned_by_search(source: str, symbol: str):
+
+def do_the_fight(humancard: Card, computercard: Card) -> None:
+    # FIXME Deactivate view? Refactor to a fixture in conftest?
+    session.setup()
+    session.view.non_blocking = True
+    session.grid[1][0] = computercard
+    session.grid[2][0] = humancard
+    handlers.play_game()
 
 
 def test_vanilla_fight():
-    # FIXME Deactivate view? Refactor to a fixture in conftest?
-
-    attacker = Card(name="Attacker", initial_power=1, initial_health=10)
-    target = Card(name="Target", initial_power=2, initial_health=3)
-    session.setup()
-    session.view.non_blocking = True
-    session.grid[1][0] = target
-    session.grid[2][0] = attacker
-    handlers.play_game()
-    assert attacker.power == 1
-    assert attacker.health == 6
+    hc = Card(name="Human Card", initial_power=2, initial_health=10)
+    cc = Card(name="Computer Card", initial_power=2, initial_health=3)
+    do_the_fight(hc, cc)
+    assert hc.power == 2
+    assert hc.health == 8
+    assert cc.power == 2
+    assert cc.health == 0
     assert session.computeragent.lives == 0
+    assert session.computeragent.health == session.computeragent.initial_health
+    # FIXME Check overflow damage == -1
     assert session.grid[1][0] is None
 
 
 def test_vanilla_with_power_0():
-    pass
+    hc = Card(name="Human Card", initial_power=0, initial_health=10)
+    cc = Card(name="Computer Card", initial_power=2, initial_health=3)
+    do_the_fight(hc, cc)
+    assert hc.health == 0
+    assert cc.health == 3
+    assert session.humanagent.lives == 0
+    assert session.grid[2][0] is None
