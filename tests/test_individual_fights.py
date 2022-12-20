bf77a63c90e0a45e0472b13b9d6e2d5c6c2fb070
ymyke <ymyke@users.noreply.github.com>
2022-12-20 22:07:53 +0100

    Add more tests

--- tests/test_individual_fights.py
+++ tests/test_individual_fights.py
@@ -32,3 +32,76 @@ def test_vanilla_with_power_0():
     assert cc.health == 3
     assert session.humanagent.lives == 0
     assert session.grid[2][0] is None
+
+
+def test_vanilla_with_no_opponent():
+    hc = Card(name="Human Card", initial_power=1, initial_health=10)
+    do_the_fight(hc, None)
+    assert hc.health == 10
+    assert session.computeragent.lives == 0
+
+
+def test_instant_death():
+    hc = Card(
+        name="Human Card",
+        initial_power=1,
+        initial_health=10,
+        sigils=[Sigil.INSTANTDEATH],
+    )
+    cc = Card(name="Computer Card", initial_power=2, initial_health=3)
+    do_the_fight(hc, cc)
+    assert hc.health == 10
+    assert cc.health == 0
+    assert session.grid[1][0] is None
+
+
+def test_soaring():
+    hc = Card(
+        name="Human Card",
+        initial_power=1,
+        initial_health=20,
+        sigils=[Sigil.SOARING],
+    )
+    cc = Card(name="Computer Card", initial_power=2, initial_health=3)
+    do_the_fight(hc, cc)
+    assert hc.health == 10
+    assert cc.health == 3
+    assert session.computeragent.lives == 0
+
+
+def test_soaring_vs_airdefense():
+    hc = Card(
+        name="Human Card",
+        initial_power=1,
+        initial_health=20,
+        sigils=[Sigil.SOARING],
+    )
+    cc = Card(
+        name="Computer Card",
+        initial_power=2,
+        initial_health=3,
+        sigils=[Sigil.AIRDEFENSE],
+    )
+    do_the_fight(hc, cc)
+    assert hc.health == 16
+    assert cc.health == 0
+    assert session.computeragent.lives == 0
+
+
+def test_soaring_and_instantdeath_vs_airdefense():
+    hc = Card(
+        name="Human Card",
+        initial_power=1,
+        initial_health=20,
+        sigils=[Sigil.SOARING, Sigil.INSTANTDEATH],
+    )
+    cc = Card(
+        name="Computer Card",
+        initial_power=2,
+        initial_health=3,
+        sigils=[Sigil.AIRDEFENSE],
+    )
+    do_the_fight(hc, cc)
+    assert hc.health == 20
+    assert cc.health == 0
+    assert session.computeragent.lives == 0
