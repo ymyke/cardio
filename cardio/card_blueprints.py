47676c6aa64b5153315317fee8b4a3bb753a7ef2
ymyke <ymyke@users.noreply.github.com>
2022-12-22 10:16:16 +0100

    Add card blueprints

---
+++ cardio/card_blueprints.py
@@ -0,0 +1,23 @@
+import copy
+from typing import List
+from . import Card, Sigil
+
+_BLUEPRINTS = [
+    Card(name="Koala", initial_power=1, initial_health=3),
+    Card(
+        name="Porcupine", initial_power=1, initial_health=2, sigils=[Sigil.AIRDEFENSE]
+    ),
+    Card(name="Lynx", initial_power=3, initial_health=2),
+    Card(name="Weasel", initial_power=1, initial_health=1),
+]
+
+
+def create_card_from_blueprint(name: str) -> Card:
+    found = [c for c in _BLUEPRINTS if c.name == name]
+    assert len(found) == 1
+    return copy.deepcopy(found[0])
+
+
+def create_cards_from_blueprint(namelist: List[str]) -> List[Card]:
+    # FIXME Use CardList type? (But has to be moved somewhere else first?)
+    return [create_card_from_blueprint(n) for n in namelist]
