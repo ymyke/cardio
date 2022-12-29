c2b5a0ebf4bd4fffdac8bc6bf5579c6ef32c45fb
ymyke <ymyke@users.noreply.github.com>
2022-12-29 10:03:31 +0100

    Refactor card renderer functions

--- code_samples/tui.py
+++ code_samples/tui.py
@@ -1,8 +1,8 @@
+from typing import Optional, List
 from asciimatics.screen import Screen
 from asciimatics.scene import Scene
-from asciimatics.effects import Cycle, Stars, Print, Mirage
+from asciimatics.effects import Effect, Print
 from asciimatics.renderers import FigletText, SpeechBubble, Box, StaticRenderer
-import asciimatics
 from asciimatics import constants
 
 from cardio import Card, GridPos
@@ -29,28 +29,47 @@ def card_to_amstring(c: Card) -> str:
     return s
 
 
-def render_card(screen, card: Card, pos: GridPos) -> list:
-    GRID_MARGIN_LEFT = 10
-    GRID_MARGIN_TOP = 10
-    BOX_WIDTH = 20
-    BOX_HEIGHT = 7
-    BOX_PADDING = 2
+BOX_WIDTH = 20
+BOX_HEIGHT = 7
+BOX_PADDING = 2
+
+
+def render_card_at(screen, card: Optional[Card], x: int, y: int) -> List[Effect]:
+    """Render card and box at x, y screen position. Draws empty box if `card`
+    is `None`.
+    """
     BOX_COLOR = Screen.COLOUR_YELLOW
+    effects = []
     pbox = Print(
-        screen,
-        Box(BOX_WIDTH, BOX_HEIGHT, uni=True, style=constants.SINGLE_LINE),
-        y=GRID_MARGIN_TOP,
-        x=GRID_MARGIN_LEFT + pos.slot * (BOX_WIDTH + BOX_PADDING),
+        screen=screen,
+        renderer=Box(BOX_WIDTH, BOX_HEIGHT, uni=True, style=constants.SINGLE_LINE),
+        x=x,
+        y=y,
         colour=BOX_COLOR,
         transparent=True,
     )
-    pcard = Print(
+    effects.append(pbox)
+    if card is not None:
+        pcard = Print(
+            screen=screen,
+            renderer=StaticRenderer(images=[card_to_amstring(card)]),
+            y=y + 1,
+            x=x + 2,
+        )
+        effects.append(pcard)
+    return effects
+
+
+def render_card_in_grid(screen, card: Optional[Card], pos: GridPos) -> List[Effect]:
+    """Render card at grid position."""
+    GRID_MARGIN_LEFT = 10
+    GRID_MARGIN_TOP = 10
+    return render_card_at(
         screen,
-        StaticRenderer(images=[card_to_amstring(card)]),
-        GRID_MARGIN_TOP + 1,  # FIXME Make this relative to box
-        GRID_MARGIN_LEFT + pos.slot * (BOX_WIDTH + BOX_PADDING) + 2,
+        card,
+        x=GRID_MARGIN_LEFT + pos.slot * (BOX_WIDTH + BOX_PADDING),
+        y=GRID_MARGIN_TOP,
     )
-    return [pbox, pcard]
 
 
 def boxdemo(screen):
@@ -84,10 +103,18 @@ def boxdemo(screen):
                 screen.height - 1,
             ),
         ]
-        + render_card(screen, create_card_from_blueprint("Porcupine"), GridPos(0, 0))
-        + render_card(screen, create_card_from_blueprint("Koala"), GridPos(0, 1))
-        + render_card(screen, create_card_from_blueprint("Weasel"), GridPos(0, 2))
-        + render_card(screen, create_card_from_blueprint("Hamster"), GridPos(0, 3))
+        + render_card_in_grid(
+            screen, create_card_from_blueprint("Porcupine"), GridPos(0, 0)
+        )
+        + render_card_in_grid(
+            screen, create_card_from_blueprint("Koala"), GridPos(0, 1)
+        )
+        + render_card_in_grid(
+            screen, create_card_from_blueprint("Weasel"), GridPos(0, 2)
+        )
+        + render_card_in_grid(
+            screen, create_card_from_blueprint("Hamster"), GridPos(0, 3)
+        )
         + boxes
     )
     scenes.append(Scene(effects, -1))
