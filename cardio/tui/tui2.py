38caab8d7d7c31129725fce096d4cf16c9ccf7b5
ymyke <ymyke@users.noreply.github.com>
2023-01-09 17:34:23 +0100

    Move comment

--- cardio/tui/tui2.py
+++ cardio/tui/tui2.py
@@ -286,7 +286,8 @@ def handle_round_of_fight(round_num, decks: Decks, computerstrategy: AgentStrate
     for pos, card in computerstrategy.cards_to_be_played(session.grid, round_num):
         print(pos, card)
         d_play_computer_card(card, pos)
-    computerstrategy.play_cards(session.grid, round_num)  # Now also place them in the model
+    # Now also place them in the model:
+    computerstrategy.play_cards(session.grid, round_num)  
 
     # Let human draw a card:
     handle_human_draws_new_card(decks)
