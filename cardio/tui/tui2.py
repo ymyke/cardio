0f0733acb5d109aab970333c3a0a9e00b5abab96
ymyke <ymyke@users.noreply.github.com>
2023-01-12 22:07:17 +0100

    Add fixme

--- cardio/tui/tui2.py
+++ cardio/tui/tui2.py
@@ -401,3 +401,5 @@ class TUIViewAndController(FightViewAndController):
 # FIXME Check if anything should be taken over from Fight.
 # FIXME Untangle what is UI/view from what is controller/business logic here.
 # FIXME Delete tui, fight, some/all of the old handlers, ...?
+# FIXME Check all the signatures in this module and in the primitives module to see if
+# type information is complete and correct.
