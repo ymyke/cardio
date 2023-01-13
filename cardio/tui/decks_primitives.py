8aeb14f18ee06d12e2502cb4c6fb0091791ab939
ymyke <ymyke@users.noreply.github.com>
2023-01-13 10:51:28 +0100

    Organize imports

--- cardio/tui/decks_primitives.py
+++ cardio/tui/decks_primitives.py
@@ -1,45 +1,44 @@
 from typing import Tuple
-from asciimatics.screen import Screen
+
+from asciimatics.constants import SINGLE_LINE
 from asciimatics.effects import Print
 from asciimatics.renderers import Box, StaticRenderer
-from asciimatics.constants import SINGLE_LINE, DOUBLE_LINE
-from .constants import *
-from .card_primitives import highlight_card_in_grid, highlight_card_at
-from .utils import dPos, show_effects
+from asciimatics.screen import Screen
+
 from cardio import GridPos
 
-def draw_handdeck_highlight(screen, slot: int, highlight: bool = True) -> None:
+from .constants import *
+from .card_primitives import highlight_card_at, highlight_card_in_grid
+from .utils import dPos, show_effects
+
+
+def draw_handdeck_highlight(screen: Screen, slot: int, highlight: bool = True) -> None:
     highlight_card_in_grid(screen, GridPos(4, slot), highlight)
-    # FIXME Call the highlight_card_in_grid directly rather than via this function?
 
 
-def draw_drawdeck_highlights(screen, highlights: Tuple[bool, bool]):
+def draw_drawdeck_highlights(screen: Screen, highlights: Tuple[bool, bool]) -> None:
     highlight_card_at(screen, dPos(DRAW_DECKS_X, DRAW_DECKS_Y), highlights[0])
     highlight_card_at(
         screen, dPos(DRAW_DECKS_X + BOX_WIDTH + 2, DRAW_DECKS_Y), highlights[1]
     )
 
 
-def draw_drawdecks(screen: Screen, counts: Tuple[int, int]):
-
-    drawcover = "    ⬆️⬆️⬆️⬆️⬆️⬆️⬆⬆️⬆️"
-    hamstercover = "      🐹🐹🐹"
-
+def draw_drawdecks(screen: Screen, counts: Tuple[int, int]) -> None:
+    DRAWCOVER = "    ⬆️⬆️⬆️⬆️⬆️⬆️⬆⬆️⬆️"
+    HAMSTERCOVER = "      🐹🐹🐹"
     show_effects(
         screen,
         [
             Print(
                 screen=screen,
-                renderer=Box(
-                    BOX_WIDTH, BOX_HEIGHT, uni=True, style=SINGLE_LINE
-                ),
+                renderer=Box(BOX_WIDTH, BOX_HEIGHT, uni=True, style=SINGLE_LINE),
                 x=DRAW_DECKS_X,
                 y=DRAW_DECKS_Y,
                 colour=Screen.COLOUR_YELLOW,
             ),
             Print(
                 screen=screen,
-                renderer=StaticRenderer(images=[drawcover]),
+                renderer=StaticRenderer(images=[DRAWCOVER]),
                 x=DRAW_DECKS_X + 1,
                 y=DRAW_DECKS_Y + BOX_HEIGHT // 2,
                 colour=Screen.COLOUR_YELLOW,
@@ -53,16 +52,14 @@ def draw_drawdecks(screen: Screen, counts: Tuple[int, int]):
             ),
             Print(
                 screen=screen,
-                renderer=Box(
-                    BOX_WIDTH, BOX_HEIGHT, uni=True, style=SINGLE_LINE
-                ),
+                renderer=Box(BOX_WIDTH, BOX_HEIGHT, uni=True, style=SINGLE_LINE),
                 x=DRAW_DECKS_X + BOX_WIDTH + 2,
                 y=DRAW_DECKS_Y,
                 colour=Screen.COLOUR_YELLOW,
             ),
             Print(
                 screen=screen,
-                renderer=StaticRenderer(images=[hamstercover]),
+                renderer=StaticRenderer(images=[HAMSTERCOVER]),
                 x=DRAW_DECKS_X + BOX_WIDTH + 2 + 1,
                 y=DRAW_DECKS_Y + BOX_HEIGHT // 2,
                 colour=Screen.COLOUR_YELLOW,
@@ -76,4 +73,3 @@ def draw_drawdecks(screen: Screen, counts: Tuple[int, int]):
             ),
         ],
     )
-
