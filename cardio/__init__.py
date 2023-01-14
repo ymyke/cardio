4d28163b727fa801dfabb525c776fe8016f90f8d
ymyke <ymyke@users.noreply.github.com>
2023-01-14 17:53:46 +0100

    Get rid of SimpleView

--- cardio/__init__.py
+++ cardio/__init__.py
@@ -2,6 +2,6 @@ from .skills import Skill
 from .card import Card, CardList
 from .deck import Deck
 from .grid import Grid, GridPos, GridPosAndCard
-from .fightvnc import FightVnC, SimpleView
+from .fightvnc import FightVnC
 from .agent import Agent
 from . import session
--- cardio/fightvnc.py
+++ cardio/fightvnc.py
@@ -1,4 +1,3 @@
-import os
 from typing import Literal
 from . import session, Card, Deck, Grid, GridPos
 from .agent_strategies import AgentStrategy
@@ -187,71 +186,3 @@ class FightVnC:
             if c.name != "Hamster"
         ]
         session.humanagent.deck.reset_cards()
-
-
-
-class SimpleView(FightVnC):
-    frames: dict = {}
-    msg: str = ""
-    non_blocking: bool = False
-
-    def update(self) -> None:
-        repr = ""
-        for i, line in enumerate(self.grid):
-            repr += f"{i}   "
-            for sloti, slot in enumerate(line):
-                frame = self.frames.get(f"{i}:{sloti}", "[]")
-                slotstr = ""
-                if slot is not None:
-                    slotstr = f"{slot.name[0:6]}|{slot.power}|{slot.health}"
-                    skillstr = "".join(s.value.symbol for s in slot.skills)
-                    slotstr = slotstr + skillstr
-                repr += f"{frame[0]}{slotstr:13s}{frame[1]}"
-            repr += "\n"
-        print(repr)
-
-        print(
-            f"\nYour health: {session.humanagent.health} | His health: {session.computeragent.health}"
-        )
-
-        print(
-            f"\nYour lives: {session.humanagent.lives} | His lives: {session.computeragent.lives}"
-        )
-
-        if self.msg != "":
-            print(f"\n{self.msg}")
-
-        print()
-        if not self.non_blocking:
-            input()
-        os.system("cls")
-
-        self.frames = {}
-        self.msg = ""
-
-    def card_activate(self, card: Card) -> None:
-        pos = self.grid.find_card(card)
-        assert pos is not None, (
-            f"{card.name} gets gets activated and "
-            "needs a view update but has no position on the grid"
-        )
-        self.frames[f"{pos.line}:{pos.slot}"] = "**"
-        self.update()
-
-    def card_getting_attacked(self, target: Card, attacker: Card) -> None:
-        pos = self.grid.find_card(target)
-        assert pos is not None, (
-            f"{target.name} gets attacked by {attacker.name} and "
-            "needs a view update but has no position on the grid"
-        )
-        self.frames[f"{pos.line}:{pos.slot}"] = "><"
-        self.update()
-
-    def human_wins_fight(self) -> None:
-        # FIXME Still necessary?
-        self.msg = "You won!"
-
-    def computer_wins_fight(self) -> None:
-        # FIXME Still necessary?
-        # QQ: Boss fights will work differently.
-        self.msg = "You lost!"
