297138442f12fc9f418555f70b3de80864e1e663
ymyke <ymyke@users.noreply.github.com>
2023-01-15 12:07:52 +0100

    Update according to latest changes

--- sandbox.py
+++ sandbox.py
@@ -2,19 +2,21 @@
 import logging
 from cardio import Card, session, GridPos
 from cardio.tui import tui
-from cardio.agent_strategies import Turn0OnlyStrategy
+from cardio.computer_strategies import Round0OnlyStrategy
 
 logging.basicConfig(level=logging.DEBUG)
 
-cs = Turn0OnlyStrategy(
-    [
+session.setup()
+
+cs = Round0OnlyStrategy(
+    grid=session.grid,
+    cards=[
         # type:ignore
-        (GridPos(0, 1), Card(name="Steed", initial_power=2, initial_health=10)),
-        (GridPos(1, 0), Card(name="Dog", initial_power=2, initial_health=5)),
-    ]
+        (GridPos(0, 1), Card(name="Steed", initial_power=1, initial_health=10)),
+        (GridPos(1, 0), Card(name="Dog", initial_power=1, initial_health=5)),
+    ],
 )
 
-session.setup()
 tv = tui.TUIViewAndController(debug=True, grid=session.grid)
 session.view = tv
 tv.handle_fight(computerstrategy=cs)
