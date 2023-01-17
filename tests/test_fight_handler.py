d3ef5a1b172b6a1e5aa6d6b6d332c0d9b97fce81
ymyke <ymyke@users.noreply.github.com>
2023-01-17 09:35:20 +0100

    Switch from agent health to common damage model and adjust tests accordingly

--- tests/test_fight_handler.py
+++ tests/test_fight_handler.py
@@ -1,12 +1,16 @@
 import copy
 from cardio import session, Card, Deck, GridPos
 from cardio.humanstrategyvnc import HumanStrategyVnC
-from cardio.computer_strategies import Round0OnlyStrategy
+from cardio.computer_strategies import Round0OnlyStrategy, PredefinedStrategy
 from cardio.card_blueprints import create_cards_from_blueprints
 
 
-def equal_logs(log1: str, log2: str) -> bool:
-    return "".join(log1.split()) == "".join(log2.split())
+def equal_logs(generatedlog: str, targetlog: str) -> bool:
+    are_equal = "".join(generatedlog.split()) == "".join(targetlog.split())
+    if not are_equal:
+        # Print the newly generated log for convenience if the logs don't match:
+        print(f"\n\nGenerated Log:\n\n{generatedlog}\n\n")
+    return are_equal
 
 
 def test_simple_initial_setup():
@@ -23,7 +27,17 @@ def test_simple_initial_setup():
     session.view.handle_fight(computerstrategy=cs)
 
     target_states_log = """\
-0:
+
+Starting round 0:
+| -           | -           | -           | -           |
+| -           | -           | -           | -           |
+| -           | -           | -           | -           |
+Hand: Kp1h3 Wp1h1 Lp3h2 Hp0h1
+Used:
+Draw: Pp1h2🚀
+Hamster: Hp0h1 Hp0h1 Hp0h1 Hp0h1 Hp0h1 Hp0h1 Hp0h1 Hp0h1 Hp0h1
+
+Starting round 1:
 | -           | -           | -           | -           |
 | Dp2h4       | Sp2h10      | -           | -           |
 | Cp1h1       | -           | -           | -           |
@@ -32,7 +46,7 @@ Used:
 Draw: Pp1h2🚀
 Hamster: Hp0h1 Hp0h1 Hp0h1 Hp0h1 Hp0h1 Hp0h1 Hp0h1 Hp0h1 Hp0h1
 
-1:
+Starting round 2:
 | -           | -           | -           | -           |
 | Dp2h3       | Sp2h10      | -           | -           |
 | -           | -           | -           | -           |
@@ -41,7 +55,7 @@ Used:
 Draw: Pp1h2🚀
 Hamster: Hp0h1 Hp0h1 Hp0h1 Hp0h1 Hp0h1 Hp0h1 Hp0h1 Hp0h1 Hp0h1
 
-2:
+Final state:
 | -           | -           | -           | -           |
 | Dp2h3       | Sp2h10      | -           | -           |
 | -           | -           | -           | -           |
@@ -50,19 +64,30 @@ Used:
 Draw: Pp1h2🚀
 Hamster: Hp0h1 Hp0h1 Hp0h1 Hp0h1 Hp0h1 Hp0h1 Hp0h1 Hp0h1 Hp0h1
 """
-
     assert equal_logs(session.view.states_log, target_states_log)
 
 
-def test_human_decks_managed_correctly():
+def test_human_gets_gems():
+    session.setup()
+    cs = PredefinedStrategy(
+        grid=session.grid,
+        cards_per_round={
+            # type: ignore
+            0: [(GridPos(1, 0), Card(name="Mouse", initial_power=1, initial_health=1))],
+            1: [(GridPos(2, 1), Card(name="Cat", initial_power=10, initial_health=1))],
+        },
+    )
+    session.view.handle_fight(computerstrategy=cs)
+    assert session.humanplayer.gems == 4
+
+
+def test_human_decks_managed_correctly(mocker):  # FIXME Should get different name?
     session.setup()
 
     original_cards = create_cards_from_blueprints(
         ["Koala", "Weasel", "Lynx", "Porcupine"]
     )
-    session.humanagent.deck = Deck(copy.deepcopy(original_cards))
-
-    session.humanagent.health = 100
+    session.humanplayer.deck = Deck(copy.deepcopy(original_cards))
     cs = Round0OnlyStrategy(
         grid=session.grid,
         cards=[
@@ -75,12 +100,22 @@ def test_human_decks_managed_correctly():
     )
 
     session.view = HumanStrategyVnC(session.grid)
+    mocker.patch("cardio.fightvnc.DAMAGE_DIFF_TO_WIN", new=50)
     session.view.handle_fight(computerstrategy=cs)
 
-    assert len(session.humanagent.deck.cards) == len(original_cards)
-
+    assert len(session.humanplayer.deck.cards) == len(original_cards)
+    assert session.humanplayer.lives == 0
     target_states_log = """
-0:
+Starting round 0:
+| -           | -           | -           | -           |
+| -           | -           | -           | -           |
+| -           | -           | -           | -           |
+Hand: Kp1h3 Wp1h1 Lp3h2 Hp0h1
+Used:
+Draw: Pp1h2🚀
+Hamster: Hp0h1 Hp0h1 Hp0h1 Hp0h1 Hp0h1 Hp0h1 Hp0h1 Hp0h1 Hp0h1
+
+Starting round 1:
 | -           | -           | -           | -           |
 | Hp2h99      | Hp2h100     | Hp2h100     | Hp2h100     |
 | Kp1h1       | -           | -           | -           |
@@ -89,7 +124,7 @@ Used: Kp1h1
 Draw: Pp1h2🚀
 Hamster: Hp0h1 Hp0h1 Hp0h1 Hp0h1 Hp0h1 Hp0h1 Hp0h1 Hp0h1
 
-1:
+Starting round 2:
 | -           | -           | -           | -           |
 | Hp2h98      | Hp2h99      | Hp2h100     | Hp2h100     |
 | -           | -           | -           | -           |
@@ -98,7 +133,7 @@ Used: Kp1h0 Wp1h0
 Draw:
 Hamster: Hp0h1 Hp0h1 Hp0h1 Hp0h1 Hp0h1 Hp0h1 Hp0h1 Hp0h1
 
-2:
+Starting round 3:
 | -           | -           | -           | -           |
 | Hp2h95      | Hp2h99      | Hp2h100     | Hp2h100     |
 | -           | -           | -           | -           |
@@ -107,7 +142,7 @@ Used: Kp1h0 Wp1h0 Lp3h0
 Draw:
 Hamster: Hp0h1 Hp0h1 Hp0h1 Hp0h1 Hp0h1 Hp0h1 Hp0h1
 
-3:
+Starting round 4:
 | -           | -           | -           | -           |
 | Hp2h95      | Hp2h99      | Hp2h100     | Hp2h100     |
 | -           | -           | -           | -           |
@@ -116,7 +151,7 @@ Used: Kp1h0 Wp1h0 Lp3h0 Hp0h0
 Draw:
 Hamster: Hp0h1 Hp0h1 Hp0h1 Hp0h1 Hp0h1 Hp0h1
 
-4:
+Starting round 5:
 | -           | -           | -           | -           |
 | Hp2h95      | Hp2h99      | Hp2h100     | Hp2h100     |
 | -           | -           | -           | -           |
@@ -125,7 +160,7 @@ Used: Kp1h0 Wp1h0 Lp3h0 Hp0h0 Hp0h0
 Draw:
 Hamster: Hp0h1 Hp0h1 Hp0h1 Hp0h1 Hp0h1
 
-5:
+Final state:
 | -           | -           | -           | -           |
 | Hp2h94      | Hp2h99      | Hp2h100     | Hp2h100     |
 | -           | -           | -           | -           |
@@ -133,105 +168,6 @@ Hand: Hp0h1 Hp0h1 Hp0h1 Hp0h1
 Used: Kp1h0 Wp1h0 Lp3h0 Hp0h0 Hp0h0 Pp1h0🚀
 Draw:
 Hamster: Hp0h1 Hp0h1 Hp0h1 Hp0h1
-
-6:
-| -           | -           | -           | -           |
-| Hp2h94      | Hp2h99      | Hp2h100     | Hp2h100     |
-| -           | -           | -           | -           |
-Hand: Hp0h1 Hp0h1 Hp0h1 Hp0h1
-Used: Kp1h0 Wp1h0 Lp3h0 Hp0h0 Hp0h0 Pp1h0🚀 Hp0h0
-Draw:
-Hamster: Hp0h1 Hp0h1 Hp0h1
-
-7:
-| -           | -           | -           | -           |
-| Hp2h94      | Hp2h99      | Hp2h100     | Hp2h100     |
-| -           | -           | -           | -           |
-Hand: Hp0h1 Hp0h1 Hp0h1 Hp0h1
-Used: Kp1h0 Wp1h0 Lp3h0 Hp0h0 Hp0h0 Pp1h0🚀 Hp0h0 Hp0h0
-Draw:
-Hamster: Hp0h1 Hp0h1
-
-8:
-| -           | -           | -           | -           |
-| Hp2h94      | Hp2h99      | Hp2h100     | Hp2h100     |
-| -           | -           | -           | -           |
-Hand: Hp0h1 Hp0h1 Hp0h1 Hp0h1
-Used: Kp1h0 Wp1h0 Lp3h0 Hp0h0 Hp0h0 Pp1h0🚀 Hp0h0 Hp0h0 Hp0h0
-Draw:
-Hamster: Hp0h1
-
-9:
-| -           | -           | -           | -           |
-| Hp2h94      | Hp2h99      | Hp2h100     | Hp2h100     |
-| -           | -           | -           | -           |
-Hand: Hp0h1 Hp0h1 Hp0h1 Hp0h1
-Used: Kp1h0 Wp1h0 Lp3h0 Hp0h0 Hp0h0 Pp1h0🚀 Hp0h0 Hp0h0 Hp0h0 Hp0h0
-Draw:
-Hamster:
-
-10:
-| -           | -           | -           | -           |
-| Hp2h94      | Hp2h99      | Hp2h100     | Hp2h100     |
-| -           | -           | -           | -           |
-Hand: Hp0h1 Hp0h1 Hp0h1
-Used: Kp1h0 Wp1h0 Lp3h0 Hp0h0 Hp0h0 Pp1h0🚀 Hp0h0 Hp0h0 Hp0h0 Hp0h0 Hp0h0
-Draw:
-Hamster:
-
-11:
-| -           | -           | -           | -           |
-| Hp2h94      | Hp2h99      | Hp2h100     | Hp2h100     |
-| -           | -           | -           | -           |
-Hand: Hp0h1 Hp0h1
-Used: Kp1h0 Wp1h0 Lp3h0 Hp0h0 Hp0h0 Pp1h0🚀 Hp0h0 Hp0h0 Hp0h0 Hp0h0 Hp0h0 Hp0h0
-Draw:
-Hamster:
-
-12:
-| -           | -           | -           | -           |
-| Hp2h94      | Hp2h99      | Hp2h100     | Hp2h100     |
-| -           | -           | -           | -           |
-Hand: Hp0h1
-Used: Kp1h0 Wp1h0 Lp3h0 Hp0h0 Hp0h0 Pp1h0🚀 Hp0h0 Hp0h0 Hp0h0 Hp0h0 Hp0h0 Hp0h0 Hp0h0
-Draw:
-Hamster:
-
-13:
-| -           | -           | -           | -           |
-| Hp2h94      | Hp2h99      | Hp2h100     | Hp2h100     |
-| -           | -           | -           | -           |
-Hand:
-Used: Kp1h0 Wp1h0 Lp3h0 Hp0h0 Hp0h0 Pp1h0🚀 Hp0h0 Hp0h0 Hp0h0 Hp0h0 Hp0h0 Hp0h0 Hp0h0 Hp0h0
-Draw:
-Hamster:
-
-14:
-| -           | -           | -           | -           |
-| Hp2h94      | Hp2h99      | Hp2h100     | Hp2h100     |
-| -           | -           | -           | -           |
-Hand:
-Used: Kp1h0 Wp1h0 Lp3h0 Hp0h0 Hp0h0 Pp1h0🚀 Hp0h0 Hp0h0 Hp0h0 Hp0h0 Hp0h0 Hp0h0 Hp0h0 Hp0h0
-Draw:
-Hamster:
-
-15:
-| -           | -           | -           | -           |
-| Hp2h94      | Hp2h99      | Hp2h100     | Hp2h100     |
-| -           | -           | -           | -           |
-Hand:
-Used: Kp1h0 Wp1h0 Lp3h0 Hp0h0 Hp0h0 Pp1h0🚀 Hp0h0 Hp0h0 Hp0h0 Hp0h0 Hp0h0 Hp0h0 Hp0h0 Hp0h0
-Draw:
-Hamster:
-
-16:
-| -           | -           | -           | -           |
-| Hp2h94      | Hp2h99      | Hp2h100     | Hp2h100     |
-| -           | -           | -           | -           |
-Hand:
-Used: Kp1h0 Wp1h0 Lp3h0 Hp0h0 Hp0h0 Pp1h0🚀 Hp0h0 Hp0h0 Hp0h0 Hp0h0 Hp0h0 Hp0h0 Hp0h0 Hp0h0
-Draw:
-Hamster:
 """
 
     assert equal_logs(session.view.states_log, target_states_log)
