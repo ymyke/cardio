970ae72eedefec86553d3e1b4c9bd7b152dc6ec7
ymyke <ymyke@users.noreply.github.com>
2023-01-25 09:27:40 +0100

    Add spirits attribute

--- cardio/human_player.py
+++ cardio/human_player.py
@@ -5,8 +5,9 @@ from .deck import Deck
 @dataclass
 class HumanPlayer:
     name: str
-    lives: int = 1
-    gems: int = 0
+    lives: int = 1  # 💓
+    gems: int = 0  # 💎
+    spirits: int = 0  # 👻 (or droplets/essence? 💧)
     deck: Deck = field(default_factory=lambda: Deck())
     # FIXME Add score, stats, ...
     # FIXME Add items
