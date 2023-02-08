4ae0e8834b621516b578309428b413dd3da63267
ymyke <ymyke@users.noreply.github.com>
2023-02-08 09:55:07 +0100

    Add `BufferedScreen`, rename module to `bufferutils`

---
+++ cardio/tui/bufferutils.py
@@ -0,0 +1,44 @@
+import copy
+from asciimatics.screen import Screen
+
+
+class BufferCopy:
+    def __init__(self, screen: Screen) -> None:
+        self.screen = screen
+        self.update()
+
+    def update(self) -> None:
+        self.buffer = copy.deepcopy(self.screen._buffer._double_buffer)
+
+    def copyback(self) -> None:
+        self.screen._buffer._double_buffer = copy.deepcopy(self.buffer)
+
+
+class BufferedScreen:
+    def __init__(self) -> None:
+        self._bufferscreen = Screen.open(unicode_aware=True)
+        self._screen = Screen.open(unicode_aware=True)
+        self._buffer_active = False
+
+    def __call__(self) -> Screen:
+        return self._bufferscreen if self._buffer_active else self._screen
+
+    def buffer_on(self) -> None:
+        self._buffer_active = True
+
+    def buffer_off(self) -> None:
+        self._buffer_active = False
+        self._screen._buffer._double_buffer = copy.deepcopy(
+            self._bufferscreen._buffer._double_buffer
+        )
+        self._screen.refresh()
+
+    def close(self) -> None:
+        self._bufferscreen.close()
+        self._screen.close()
+
+    def __enter__(self):
+        self.buffer_on()
+
+    def __exit__(self, *args):
+        self.buffer_off()
