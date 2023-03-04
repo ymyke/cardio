252d3c1f266b3940644833b8da2c3bcad325a5a4
ymyke <ymyke@users.noreply.github.com>
2023-03-04 11:44:47 +0100

    Revamp tests to use the new `ProperlyPlacingHumanStrategyVnC`
    Which makes the tests more comprehensive.

--- tests/test_skills.py
+++ tests/test_skills.py
@@ -1,25 +1,34 @@
-from typing import List, Optional
-from cardio import Card, Skill, session
+from typing import Optional
+import pytest
+from cardio import Card, CardList, Skill, session, GridPos
 from cardio.computer_strategies import Round0OnlyStrategy
+from cardio.humanstrategyvnc import ProperlyPlacingHumanStrategyVnC
 
 
-def do_the_fight(
-    humancard: Optional[Card],
-    computercard: Optional[Card],
-    playerhand: Optional[List[Card]] = None,
-) -> None:
+@pytest.fixture(autouse=True)
+def never_reset_cards(mocker):
+    """Do not reset cards anywhere in this module so we can verify the effects of
+    fights.
+    """
+    mocker.patch("cardio.Deck.reset_cards")
+
+
+def do_the_fight(humancards: CardList, computercard: Optional[Card]) -> None:
+    """Note that the assumption here is that `ProperlyPlacingHumanStrategyVnC` will
+    place new cards on the first free slot from the left, i.e., the very first human
+    card gets placed on (2,0).
+    """
     session.setup()
-    session.humanplayer.deck.cards = playerhand or []
-    cs = Round0OnlyStrategy(
-        grid=session.grid, cards=[((1, 0), computercard), ((2, 0), humancard)]  # type: ignore
-    )
+    session.humanplayer.deck.cards = humancards
+    cs = Round0OnlyStrategy(grid=session.grid, cards=[(GridPos(1, 0), computercard)])
+    session.view = ProperlyPlacingHumanStrategyVnC(session.grid, whichrounds=[0])
     session.view.handle_fight(computerstrategy=cs)
 
 
 def test_vanilla_fight():
     hc = Card("Human Card", 2, 10, 1)
     cc = Card("Computer Card", 2, 3, 1)
-    do_the_fight(hc, cc)
+    do_the_fight([hc], cc)
     assert hc.power == 2
     assert hc.health == 8
     assert cc.power == 2
@@ -34,7 +43,7 @@ def test_vanilla_with_power_0():
     """
     hc = Card("Human Card", 0, 10, 1)
     cc = Card("Computer Card", 2, 3, 1)
-    do_the_fight(hc, cc)
+    do_the_fight([hc], cc)
     assert hc.health == 10  # full health remaining
     assert cc.health == 3
     assert session.humanplayer.lives == 0
@@ -49,7 +58,7 @@ def test_vanilla_with_power_0_and_additional_card_in_hand():
     hc = Card("Human Card", 0, 10, 1)
     hc2 = Card("Human Card 2", 1, 10, 1)
     cc = Card("Computer Card", 2, 3, 1)
-    do_the_fight(hc, cc, playerhand=[hc2])
+    do_the_fight([hc, hc2], cc)
     assert hc.health == 0  # health depleted
     assert cc.health == 3
     assert session.humanplayer.lives == 0
@@ -58,14 +67,14 @@ def test_vanilla_with_power_0_and_additional_card_in_hand():
 
 def test_vanilla_with_no_opponent():
     hc = Card("Human Card", 1, 10, 1)
-    do_the_fight(hc, None)
+    do_the_fight([hc], None)
     assert hc.health == 10
 
 
 def test_instant_death():
     hc = Card("Human Card", 1, 10, 1, skills=[Skill.INSTANTDEATH])
     cc = Card("Computer Card", 2, 3, 1)
-    do_the_fight(hc, cc)
+    do_the_fight([hc], cc)
     assert hc.health == 10
     assert cc.health == 0
     assert session.grid[1][0] is None
@@ -74,7 +83,7 @@ def test_instant_death():
 def test_soaring():
     hc = Card("Human Card", 1, 20, 1, skills=[Skill.SOARING])
     cc = Card("Computer Card", 2, 3, 1)
-    do_the_fight(hc, cc)
+    do_the_fight([hc], cc)
     # 12 not 10, bc fight-over conditions are checked after each line gets activated:
     assert hc.health == 12
     assert cc.health == 3
@@ -84,7 +93,7 @@ def test_soaring():
 def test_soaring_vs_airdefense():
     hc = Card("Human Card", 1, 20, 1, skills=[Skill.SOARING])
     cc = Card("Computer Card", 2, 3, 1, skills=[Skill.AIRDEFENSE])
-    do_the_fight(hc, cc)
+    do_the_fight([hc], cc)
     assert hc.health == 16
     assert cc.health == 0
     assert session.grid[1][0] is None
@@ -93,7 +102,7 @@ def test_soaring_vs_airdefense():
 def test_soaring_and_instantdeath_vs_airdefense():
     hc = Card("Human Card", 1, 20, 1, skills=[Skill.SOARING, Skill.INSTANTDEATH])
     cc = Card("Computer Card", 2, 3, 1, skills=[Skill.AIRDEFENSE])
-    do_the_fight(hc, cc)
+    do_the_fight([hc], cc)
     assert hc.health == 20
     assert cc.health == 0
     assert session.grid[1][0] is None
@@ -105,7 +114,7 @@ def test_soaring_and_instantdeath_vs_no_airdefense():
     """
     hc = Card("Human Card", 1, 20, 1, skills=[Skill.SOARING, Skill.INSTANTDEATH])
     cc = Card("Computer Card", 2, 3, 1)
-    do_the_fight(hc, cc)
+    do_the_fight([hc], cc)
     # 12 not 10, bc fight-over conditions are checked after each line gets activated:
     assert hc.health == 12
     assert cc.health == 3
@@ -115,7 +124,7 @@ def test_soaring_and_instantdeath_vs_no_airdefense():
 def test_spines():
     hc = Card("Human Card", 2, 10, 1)
     cc = Card("Computer Card", 2, 3, 1, skills=[Skill.SPINES])
-    do_the_fight(hc, cc)
+    do_the_fight([hc], cc)
     assert hc.health == 6
     assert cc.health == 0
     assert session.grid[1][0] is None
@@ -124,7 +133,7 @@ def test_spines():
 def test_spines_resulting_in_both_cards_dying_simultaneously():
     hc = Card("Human Card", 1, 1, 1)
     cc = Card("Computer Card", 0, 1, 1, skills=[Skill.SPINES])
-    do_the_fight(hc, cc)
+    do_the_fight([hc], cc)
     assert hc.health == 0
     assert cc.health == 0
     assert session.grid[1][0] is None
