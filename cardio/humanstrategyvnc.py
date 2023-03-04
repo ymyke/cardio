f2fc389b4c8353510d1cee6f7ae1ae0e0355a6a9
ymyke <ymyke@users.noreply.github.com>
2023-03-04 11:40:36 +0100

    Add `ProperlyPlacingHumanStrategyVnC`

--- cardio/humanstrategyvnc.py
+++ cardio/humanstrategyvnc.py
@@ -1,7 +1,8 @@
 import logging
-from typing import Callable
+from typing import Callable, List, Optional
+from . import Deck, GridPos, Grid
 from .fightvnc import FightVnC
-from . import Deck
+from .placement_manager import PlacementManager
 
 
 class HumanStrategyVnC(FightVnC):
@@ -27,5 +28,32 @@ class HumanStrategyVnC(FightVnC):
                 self.grid[2][slot] = card
                 self.decks.useddeck.add_card(card)
                 logging.debug("Human plays %s on %s", card.name, slot)
+                return
+        logging.debug("Human plays no card.")
+
+
+class ProperlyPlacingHumanStrategyVnC(HumanStrategyVnC):
+    def __init__(
+        self, grid: Grid, whichrounds: Optional[List[int]] = None, *args, **kwargs
+    ) -> None:  # TODO Can I this w/o the grid here?
+        """Use `whichrounds` to only become active in specific fight rounds."""
+        super().__init__(grid, *args, **kwargs)
+        self.whichrounds = whichrounds
+
+    def handle_human_plays_cards(self, place_card_callback: Callable) -> None:
+        if self.whichrounds and not self.round_num in self.whichrounds:
+            return
+        if self.decks.handdeck.is_empty():
+            return
+        for slot in range(self.grid.width):
+            pos = GridPos(2, slot)
+            if self.grid.get_card(pos) is None:
+                card = self.decks.handdeck.cards[0]
+                card.costs_fire = 0
+                card.costs_spirits = 0
+                p = PlacementManager(self.grid, 0, card)
+                p.target_card = card  # TODO Add these to initializer
+                p.placement_position = pos  # TODO Rename to target_position
+                place_card_callback(p, 0)
                 return
         logging.debug("Human plays no card.")
