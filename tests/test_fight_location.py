58936b76ca5e8212a47795dcc737b86cc485a613
ymyke <ymyke@users.noreply.github.com>
2023-04-10 13:21:01 +0200

    Fix test

--- tests/test_fight_location.py
+++ tests/test_fight_location.py
@@ -4,19 +4,17 @@ from cardio.computer_strategies import Round0OnlyStrategy
 
 
 def test_generate():
-    l = FightLocation(0, 0, 0)
+    l = FightLocation("0", 0, 0, [0])
     assert isinstance(l.grid, Grid)
     assert isinstance(l.computerstrategy, Round0OnlyStrategy)
     assert [(loc, card.name) for loc, card in l.computerstrategy.cards] == [
-        (GridPos(line=0, slot=1), "Lynx"),
-        (GridPos(line=0, slot=0), "Hamster"),
-        (GridPos(line=1, slot=0), "Porcupine"),
-        (GridPos(line=1, slot=1), "Weasel"),
+        (GridPos(line=0, slot=1), "Weasel"),
+        (GridPos(line=0, slot=2), "Koala"),
     ]
 
     # Try different seed:
-    l = FightLocation(0, 10, 1)
+    l = FightLocation("0", 10, 1, [0])
     assert [(loc, card.name) for loc, card in l.computerstrategy.cards] == [
-        (GridPos(line=0, slot=3), "Weasel"),
-        (GridPos(line=1, slot=2), "Porcupine"),
+        (GridPos(line=1, slot=1), "Church Mouse"),
+        (GridPos(line=0, slot=2), "Koala"),
     ]
