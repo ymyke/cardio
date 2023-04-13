a10c81c3beb797b3da87540a5c5f55684f3b2cc8
ymyke <ymyke@users.noreply.github.com>
2023-04-13 09:06:54 +0200

    Add todo

--- cardio/tui/tui.py
+++ cardio/tui/tui.py
@@ -5,6 +5,8 @@ All ready-only methods to update the view, show animations, or handle user inter
 of game-related logic must take place or be orchestrated in FightVnC.
 """
 
+# TODO Rename this module to tuifightvnc.py
+
 import atexit
 import itertools
 from typing import Callable, List, Optional, Tuple
