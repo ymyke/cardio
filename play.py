34df6433d5bbdb40b21b766567f53ea614050a6c
ymyke <ymyke@users.noreply.github.com>
2023-04-14 13:04:37 +0200

    Complete game loop: Game is now basically working!! 😱

--- play.py
+++ play.py
@@ -29,19 +29,8 @@ while game_on:
     chosen_loc = mapview.get_next_location()
     mapview.move_to(chosen_loc)
     run.move_to(chosen_loc)
-    # game_on = chosen_loc.handle() TODO
+    game_on = chosen_loc.handle()
 
-# Going through the map:
-# - Show map and mark current location. (Which is always on the lowest line in the map.)
-# - Disable non-accessible paths (e.g. in dark gray) OR: Make everything dark grey and
-#   highlight: current location, next locations to be chosen from, currently highlighted
-#   next location, all accessible locations.
-# - Mark all possible targets for the next location and place the cursor in the leftmost
-#   of those.
-# - Allow user to choose and confirm next location.
-# - Visit/handle selected location.
-# - Increase current_rung
-# - Start from top of loop.
 
 ### Game over:
 ###   ...
