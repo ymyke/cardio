5cf8a738612620c67158493e0ac56d8865cc6374
ymyke <ymyke@users.noreply.github.com>
2023-04-18 07:03:11 +0200

    Add tests for upgrader locations

---
+++ tests/test_upgrader_location.py
@@ -0,0 +1,55 @@
+from cardio import Card, Deck, gg
+from cardio.locations.upgrader_location import (
+    PowerUpgraderLocation,
+    HealthUpgraderLocation,
+    PowerUpgraderMultiLocation,
+    UpgraderView,
+)
+
+
+class FakeUpgraderView(UpgraderView):
+    def __init__(self, *args, **kwargs) -> None:
+        ...
+
+    def pick(self) -> Card:
+        return gg.humanplayer.deck.cards[0]
+
+    def show_destroy(self, card: Card) -> None:
+        ...
+
+    def show_upgrade(self, card: Card) -> None:
+        ...
+
+    def ask(self, card: Card) -> bool:
+        return True
+
+    def close(self) -> None:
+        ...
+
+
+def test_powerupgraderlocation(gg_setup):
+    card = Card("X", 1, 1, 1, None)
+    gg.humanplayer.deck = Deck("main", [card])
+    loc = PowerUpgraderLocation("0", 0, 0, [])
+    loc.handle(view_class=FakeUpgraderView)
+    assert card.power == 2
+    assert card.initial_power == 2
+    assert gg.humanplayer.deck.size() == 1
+
+
+def test_healthupgraderlocation(gg_setup):
+    card = Card("X", 1, 1, 1, None)
+    gg.humanplayer.deck = Deck("main", [card])
+    loc = HealthUpgraderLocation("0", 0, 0, [])
+    loc.handle(view_class=FakeUpgraderView)
+    assert card.health == 2
+    assert card.initial_health == 2
+    assert gg.humanplayer.deck.size() == 1
+
+
+def test_powerupgraderlocation_multi(gg_setup):
+    card = Card("X", 1, 1, 1, None)
+    gg.humanplayer.deck = Deck("main", [card])
+    loc = PowerUpgraderMultiLocation("0", 0, 0, [])
+    loc.handle(view_class=FakeUpgraderView)
+    assert gg.humanplayer.deck.is_empty()
