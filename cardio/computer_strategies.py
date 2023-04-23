5f3af656ce328d3b67765905c3d0e09d3421f6af
ymyke <ymyke@users.noreply.github.com>
2023-04-23 18:21:04 +0200

    Add todo

--- cardio/computer_strategies.py
+++ cardio/computer_strategies.py
@@ -5,18 +5,19 @@ from . import Grid, GridPosAndCard
 
 class ComputerStrategy(ABC):
     """Abstract base class for computer strategies.
-    
+
     A computer strategy defines when the computer brings which cards (or maybe in the
-    future also other things, such as items) into play. 
+    future also other things, such as items) into play.
 
     A strategy can take some input parameters (initial deck, current grid, turn number,
-    ...) and return which cards will be played to which slots. 
-  
+    ...) and return which cards will be played to which slots.
+
     It is conceivable that a strategy is as constrained or free as necessary: a)
     strategy adheres to no rules at all, b) strategy needs to work within all the rules
     (e.g., placement rules such as fire/spirit costs etc.), c) strategy needs to work
     with an initial deck.
     """
+
     def __init__(self, grid: Grid) -> None:
         self.grid = grid
 
@@ -48,13 +49,16 @@ class Round0OnlyStrategy(ComputerStrategy):
         for (line, slot), card in self.cards_to_be_played(round_number):
             self.grid.lines[line][slot] = card
 
+
 class PredefinedStrategy(ComputerStrategy):
     """Simply plays predefined cards in specific rounds of a fight. Does not perform any
-    checks whether the grid is empty or not where a card should be played. 
+    checks whether the grid is empty or not where a card should be played.
     # FIXME Add that check and test it too.
     """
 
-    def __init__(self, cards_per_round: Dict[int, List[GridPosAndCard]], *args, **kwargs) -> None:
+    def __init__(
+        self, cards_per_round: Dict[int, List[GridPosAndCard]], *args, **kwargs
+    ) -> None:
         super().__init__(*args, **kwargs)
         self.cards_per_round = cards_per_round
 
@@ -64,3 +68,7 @@ class PredefinedStrategy(ComputerStrategy):
     def play_cards(self, round_number: int) -> None:
         for (line, slot), card in self.cards_to_be_played(round_number):
             self.grid.lines[line][slot] = card
+
+
+# TODO watch out! After the first round, computer strategy should place cards only in
+# the prep line, not in line 1 directly.
