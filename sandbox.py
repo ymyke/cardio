8e2722d8a7c0a803c7218d76cadce218697fecb3
ymyke <ymyke@users.noreply.github.com>
2023-04-24 11:00:51 +0200

    Clean up sandbox

--- sandbox.py
+++ sandbox.py
@@ -1,25 +1,4 @@
-# %%
-import logging
-from cardio import Card, gg, GridPos
-from cardio.tui.locations import fightview
-from cardio.computer_strategies import Round0OnlyStrategy
+#%% Print potency stats:
+from cardio.potency_utils import print_potency_stats
 
-logging.basicConfig(level=logging.DEBUG)
-
-gg.setup()
-
-cs = Round0OnlyStrategy(
-    grid=gg.grid,
-    cards=[
-        # type:ignore
-        (GridPos(0, 1), Card("Steed", 1, 10, 1)),
-        (GridPos(1, 0), Card("Dog", 1, 5, 1)),
-        (GridPos(0, 0), Card("Mouse", 1, 1, 1)),
-    ],
-)
-
-tv = fightview.TUIFightVnC(debug=True, computerstrategy=cs, grid=gg.grid)
-gg.vnc = tv
-gg.humanplayer.spirits = 3
-tv.handle_fight()
-tv.close()
+print_potency_stats()
