5045dd4a09963de561660515c6be4a6329216809
ymyke <ymyke@users.noreply.github.com>
2023-05-29 10:34:51 +0200

    Move `create_new` and hamster blueprint to `HumanPlayer`

--- cardio/human_player.py
+++ cardio/human_player.py
@@ -1,5 +1,12 @@
+from __future__ import annotations
 from dataclasses import dataclass, field
+from typing import TYPE_CHECKING
 from .deck import Deck
+
+# from cardio.blueprints import thecatalog # Imported below to avoid circular imports
+
+if TYPE_CHECKING:
+    from .blueprints import Blueprint
 
 
 @dataclass
@@ -10,3 +17,21 @@ class HumanPlayer:
     spirits: int = 0  # 👻 (or droplets/essence? 💧)
     deck: Deck = field(default_factory=lambda: Deck("main"))
     collection: Deck = field(default_factory=lambda: Deck("collection"))
+    hamster_blueprint: Blueprint = None  # type: ignore
+
+    def __post_init__(self):
+        from cardio.blueprints import thecatalog
+
+        if not self.hamster_blueprint:
+            self.hamster_blueprint = thecatalog.get("Hamster")
+
+    @classmethod
+    def create_new(cls, name: str) -> HumanPlayer:
+        from cardio.blueprints import thecatalog
+
+        p = cls(name=name, lives=1, gems=0, spirits=3)
+        start_cards = thecatalog.find_by_potency_range(3, 4).instantiate()
+        start_cards = [c for c in start_cards if c.costs_fire + c.costs_spirits <= 2]
+        # FIXME ^ Better use net potency here too.
+        p.collection.cards = start_cards
+        return p
