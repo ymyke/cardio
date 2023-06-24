1bc3e4c9dd7a819c6eba78148118174549ac18e9
ymyke <ymyke@users.noreply.github.com>
2023-06-24 21:28:04 +0200

    Fix test for changed potencies

--- tests/test_fight_location.py
+++ tests/test_fight_location.py
@@ -8,15 +8,15 @@ def test_generate():
     assert isinstance(l.grid, Grid)
     assert isinstance(l.computerstrategy, Round0OnlyStrategy)
     assert [(loc, card.name) for loc, card in l.computerstrategy.cards] == [
-        (GridPos(line=0, slot=1), "Fireshrew"),
-        (GridPos(line=0, slot=2), "Skysting"),
+        (GridPos(line=0, slot=1), "Hammerling"),
+        (GridPos(line=0, slot=2), "Shattershell"),
     ]
 
     # Try different seed and higher rung:
     l = FightLocation("2", 30, 1, [0])
     assert [(loc, card.name) for loc, card in l.computerstrategy.cards] == [
-        (GridPos(line=0, slot=0), "Butterbat"),
-        (GridPos(line=1, slot=3), "Seedling"),
-        (GridPos(line=1, slot=0), "Luckbird"),
-        (GridPos(line=0, slot=2), "Firechick"),
+        (GridPos(line=0, slot=0), "Seedar"),
+        (GridPos(line=1, slot=3), "Emberling"),
+        (GridPos(line=1, slot=0), "Ghostowl"),
+        (GridPos(line=0, slot=2), "Hoardling"),
     ]
