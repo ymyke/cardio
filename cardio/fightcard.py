19bcbdffea8f936c9624605cf4dde911d4bd20be
ymyke <ymyke@users.noreply.github.com>
2023-06-24 11:03:16 +0200

    Add Weakness skill

--- cardio/fightcard.py
+++ cardio/fightcard.py
@@ -247,7 +247,10 @@ class FightCard(Card):
         # ----- Deal damage -----
 
         # Damage to target:
-        target_damage_left = target.take_damage(attacker_power)
+        damage_to_target = attacker_power
+        if sk.Weakness in self.skills:
+            damage_to_target = self.skills.get(sk.Weakness).modify_damage(damage_to_target)  # type: ignore
+        target_damage_left = target.take_damage(damage_to_target)
         if target_damage_left > 0:
             self.vnc.handle_agent_damage(target_player, target_damage_left)
 
