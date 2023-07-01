6a16c63623b69f7b559593b7428d9582d8532039
ymyke <ymyke@users.noreply.github.com>
2023-07-01 11:53:29 +0200

    Simplify fight location test

--- tests/test_fight_location.py
+++ tests/test_fight_location.py
@@ -1,20 +1,9 @@
-from cardio import Grid, GridPos
+from cardio import Grid
 from cardio.locations.fight_location import FightLocation
-from cardio.computer_strategies import Round0OnlyStrategy
+from cardio.computer_strategies import RungBasedStrategy
 
 
 def test_generate():
     l = FightLocation("0", 0, 0, [0])
     assert isinstance(l.grid, Grid)
-    assert isinstance(l.computerstrategy, Round0OnlyStrategy)
-    assert [(loc, card.name) for loc, card in l.computerstrategy.cards] == [
-        (GridPos(line=0, slot=1), "Smokepuff"),
-        (GridPos(line=0, slot=0), "Firefly"),
-        (GridPos(line=1, slot=0), "Crabster"),
-    ]
-
-    # Try different seed and higher rung:
-    l = FightLocation("2", 30, 1, [0])
-    assert [(loc, card.name) for loc, card in l.computerstrategy.cards] == [
-        (GridPos(line=1, slot=1), "Fritter")
-    ]
+    assert isinstance(l.computerstrategy, RungBasedStrategy)
